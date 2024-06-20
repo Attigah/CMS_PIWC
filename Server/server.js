35a2b86d8e46945652d6eb0eda24850be361e732
Attigah <bkeattigah@st.ug.edu.gh>
2024-06-20 16:45:59 +0000

    Removed all codes to start project afresh

--- Server/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-require("dotenv").config();
-// Create server
-app_1.app.listen(process.env.PORT, () => {
-    console.log(`Server is connected with port ${process.env.PORT}`);
-});
--- Server/server.ts
+++
@@ -1,7 +0,0 @@
-import { app } from "./app";
-require("dotenv").config();
-
-// Create server
-app.listen(process.env.PORT, () => {
-  console.log(`Server is connected with port ${process.env.PORT}`);
-});

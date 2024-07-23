f888fa4286b462a02886164f435203ce44607133
Attigah <bkeattigah@st.ug.edu.gh>
2024-07-23 20:34:48 +0000

    React lessons setup

---
+++ abk-blog/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+
+    <div classnName="ThisIsGoat">
+      <p>
+        How is this possible
+      </p>
+      
+
+    </div> 
+
+    </div>
+  );
+}
+
+export default App;

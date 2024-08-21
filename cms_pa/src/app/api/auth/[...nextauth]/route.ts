dcf10af32d990baf5c062a04aeff30859f823721
Attigah <bkeattigah@st.ug.edu.gh>
2024-08-21 02:34:13 +0000

    login successfully created, login is fully functional with no error

--- cms_pa/src/app/api/auth/[...nextauth]/route.ts
+++ cms_pa/src/app/api/auth/[...nextauth]/route.ts
@@ -1,39 +1,38 @@
 import NextAuth from "next-auth";
-import {Account, User as AuthUser} from "next-auth";
+import { Account, User as AuthUser } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 import User from "@/models/User";
 import connect from "@/utils/db";
 
-export const authOptions:any = {
+export const authOptions: any = {
   // Configure one or more authentication providers
   providers: [
-    CredentialsProvider ({
-        id: "credentials",
-        name: "credentials",
-        credentials:{
-            email: {label: "Email", type:"text"},
-            password: {label: "Password", type: "password"},
-        },
-        async authorize(credentials: any) {
-            await connect();
-            try {
-                const user = await User.findOne({email: credentials.email});
-                if(user) {
-                    const isPasswordCorrect = await bcrypt.compare(
-                        credentials.password,
-                        user.password
-                    )
-                    if (isPasswordCorrect) {
-                        return user;
-                    }
-                }
-            }catch (err) {
-                throw new Error(err);
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials: any) {
+        await connect();
+        try {
+          const user = await User.findOne({ email: credentials.email });
+          if (user) {
+            const isPasswordCorrect = await bcrypt.compare(
+              credentials.password,
+              user.password
+            );
+            if (isPasswordCorrect) {
+              return user;
             }
+          }
+        } catch (err: any) {
+          throw new Error(err);
         }
-
+      },
     }),
     GithubProvider({
       clientId: process.env.GITHUB_ID ?? "",
@@ -41,6 +40,32 @@ export const authOptions:any = {
     }),
     // ...add more providers here
   ],
-}
+  callbacks: {
+    async signIn({ user, account }: { user: AuthUser; account: Account }) {
+      if (account?.provider == "credentials") {
+        return true;
+      }
+      if (account?.provider == "github") {
+        await connect();
+        try {
+          const existingUser = await User.findOne({ email: user.email });
+          if (!existingUser) {
+            const newUser = new User({
+              email: user.email,
+            });
 
-export default NextAuth(authOptions)
+            await newUser.save();
+            return true;
+          }
+          return true;
+        } catch (err) {
+          console.log("Error saving user", err);
+          return false;
+        }
+      }
+    },
+  },
+};
+
+export const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
